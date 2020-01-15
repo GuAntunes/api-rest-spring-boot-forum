07781bb46738e408567f575c3ffcb672e5902c5a
Gustavo Antunes <gas.antunes84@gmail.com>
2020-01-15 19:14:24 -0300

    Utilizando Cache

--- src/main/java/com/gustavoantunes/forum/ForumApplication.java
+++ src/main/java/com/gustavoantunes/forum/ForumApplication.java
@@ -2,8 +2,15 @@ package com.gustavoantunes.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+//Esta anotação permite que Pageable seja utilizado sendo passado como 
+//parâmentro de um método
+@EnableSpringDataWebSupport
+//Para hábilitar o serviço de Cache
+@EnableCaching
 public class ForumApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/gustavoantunes/forum/controller/TopicosController.java
+++ src/main/java/com/gustavoantunes/forum/controller/TopicosController.java
@@ -8,10 +8,13 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -52,10 +55,18 @@ public class TopicosController {
 	// o parâmetro required false @RequestParam(required = false)
 
 	@GetMapping
-	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina,
-			@RequestParam int qtd, @RequestParam String ordenacao) {
+	@Cacheable(value = "listaDeTopicos")
+	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, 
+			@PageableDefault(sort = "id", direction = Direction.ASC)Pageable paginacao) {
+		//Exemplo de requisição para Pageable como parâmetro do método
+		//http://localhost:8080/topicos?page=0&size=10&sort=id,asc&sort=dataCriacao,desc
+		
+//		@PageableDefault(sort = "id", direction = Direction.ASC)
+//		Com este comando é possível setar valores padrões para a consulta
+		
 
-		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC, ordenacao);
+		//Forma manual de utilizar a classe
+//		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC, ordenacao);
 
 		Page<Topico> topicos;
 		if (nomeCurso == null) {
@@ -68,6 +79,10 @@ public class TopicosController {
 
 	@PostMapping
 	@Transactional
+	// O cacheEvict informa ao spring quando um determinado cache deve ser inválidado,
+	// com o parâmetro value = listaDeTopicos é informado qual o cache que ele deve inválidar,
+	// o parâmetro allEntries, informa se é pra inválidar todos os registros deste cache
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -91,6 +106,7 @@ public class TopicosController {
 	// ou save, pois ao final do método as modificações serão
 	// realizadas automaticamente no banco de dados
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id, @RequestBody AtualizacaoTopicoFormDTO form) {
 		// A utilização do Optional impede que o código lance uma exception,
 		// Assim podemos validar posteriormente caso não exista o registro na base
@@ -104,6 +120,7 @@ public class TopicosController {
 
 	@DeleteMapping("{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<?> remover(@PathVariable Long id) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if (optional.isPresent()) {
